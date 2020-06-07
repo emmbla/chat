0ea7353757d865dd8ceec3bbacfb4c5af505dc60
embla <emblajonsson@gmail.com>
2020-06-07 15:45:31 +0200

    JOIN

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-export default class Join extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Join</h1>
+const Join = () => {
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+  //   const [password, setPassword] = useState('')
+
+  return (
+    <div className="main-container">
+      <div className="join-content">
+        <h1 className="title">Join</h1>
+        <div>
+          <input
+            placeholder="Name"
+            onChange={(event) => setName(event.target.value)}
+            className="joinInput"
+            type="text"
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Room"
+            onChange={(event) => setRoom(event.target.value)}
+            className="joinInput mt-20"
+            type="text"
+          />
+        </div>
+        <Link to={'/chat?name=${name}&room=${room}'}>
+          <button
+            onClick={(event) =>
+              !name || !room ? event.preventDefault() : null
+            }
+            className="btn mt-20"
+            type="submit"
+          >
+            Sign in
+          </button>
+        </Link>
       </div>
-    )
-  }
+    </div>
+  )
 }
+export default Join
